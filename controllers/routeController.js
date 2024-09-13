4c7cf88e4b0663579be25cfcf9921448c2c77e1f
joaocastro <joaocastro0429@gmail.com>
2024-09-12 23:17:14 -0300

    update na api

--- controllers/routeController.js
+++ controllers/routeController.js
@@ -2,3 +2,23 @@ exports.hello=(req,res)=>{
    return  res.send('Hello World')
 }
 
+exports.create=(req,res,next)=>{
+    res.json(req.body)
+
+}
+
+exports.listAll=(req,res,next)=>{
+    res.json({message:'List all users'})
+}
+
+exports.getUser=(req,res,next)=>{
+    res.json({message:'Get user by id'})
+}
+
+exports.update=(req,res,next)=>{
+   res.json({message:'Update user by id'})
+}
+
+exports.delete=(req,res,next)=>{
+    res.json({message:'Delete user by id'})
+}
--- routes/route.js
+++ routes/route.js
@@ -6,6 +6,10 @@ module.exports=app=>{
     
     app.get('/',routerController.hello)
     app.post('/create',routerController.create)
+    app.get('/list',routerController.listAll)
+    app.get('/user/:id',routerController.getUser)
+    app.put('/user/:id',routerController.update)
+    app.delete('/user/:id',routerController.delete)
      
 
 
