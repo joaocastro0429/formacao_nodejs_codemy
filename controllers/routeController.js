3c908b92c37d72b7b0bd1f01c88328321954f6d9
joaocastro <joaocastro0429@gmail.com>
2024-09-13 21:19:39 -0300

    add try/catch

--- controllers/routeController.js
+++ controllers/routeController.js
@@ -1,95 +1,90 @@
 const users = [
-    {
-      id: 1,
-      firstname: "Alice",
-      lastname: "Smith",
-      
-    },
-    {
-      id: 2,
-      firstname: "Bob",
-      lastname: "Johnson",
-     
-    },
-    {
-      id: 3,
-      firstname: "Charlie",
-      lastname: "Brown",
-     
-    },
-    {
-      id: 4,
-      firstname: "Diana",
-      lastname: "Wilson",
-      
-    },
-    {
-      id: 5,
-      firstname: "Eve",
-      lastname: "Davis",
-     
-    }
-  ];
-    
+    { id: 1, firstname: "Alice", lastname: "Smith" },
+    { id: 2, firstname: "Bob", lastname: "Johnson" },
+    { id: 3, firstname: "Charlie", lastname: "Brown" },
+    { id: 4, firstname: "Diana", lastname: "Wilson" },
+    { id: 5, firstname: "Eve", lastname: "Davis" }
+];
 
-exports.hello=(req,res)=>{
-   return  res.send('Hello World')
+
+exports.hello = (req, res) => {
+    return res.send('Hello World')
 }
 
 exports.create = (req, res, next) => {
-    const { firstname, lastname } = req.body;
-  
-    // Gerando um novo ID com base no comprimento do array + 1
-    const id = users.length + 1;
-  
-    // Criando o novo usuário
-    const user = { id, firstname, lastname };
-  
-    console.log(user);
-  
-    users.push(user);
-  
-    res.status(201).json(user);
-  };
+    try {
+        const { firstname, lastname } = req.body;
+        // Gerando um novo ID com base no comprimento do array + 1
+        const id = users.length + 1;
 
-exports.listAll=(req,res,next)=>{
+        users.push(user);
+
+        res.status(201).json(user);
+
+    } catch (erro) {
+        res.status(500).json({ message: 'Internal Server Error', error: erro.message })
+
+    }
+
+
+};
+
+exports.listAll = (req, res, next) => {
     res.json(users)
 }
 
-exports.getUser=(req,res,next)=>{
-    const id = parseInt(req.params.id)
-    const user = users.find(user => user.id===id)
-    if(!user) return res.status(404).json({message:'User not found'})
-    res.json(user)
-    
+exports.getUser = (req, res, next) => {
+    try {
+        const id = parseInt(req.params.id)
+        const user = users.find(user => user.id === id)
+        if (!user) return res.status(404).json({ message: 'User not found' })
+
+        res.json(user)
+
+
+    } catch (error) {
+        res.status(500).json({ message: 'Internal Server Error', error: error.message })
+
+    }
+
+
 }
 
 exports.update = (req, res, next) => {
-    const { firstname, lastname } = req.body;
-    const id = parseInt(req.params.id);
+    try {
+        const { firstname, lastname } = req.body;
+        const id = parseInt(req.params.id);
+        // Encontrar o índice do usuário com o ID fornecido
+        const userIndex = users.findIndex(user => user.id === id);
+        if (userIndex === -1) {
+            return res.status(404).json({ message: 'User not found' });
+        }
 
-    // Encontrar o índice do usuário com o ID fornecido
-    const userIndex = users.findIndex(user => user.id === id);
+        // Atualizar os campos do usuário
+        users[userIndex].firstname = firstname;
+        users[userIndex].lastname = lastname;
 
-    // Verificar se o usuário foi encontrado
-    if (userIndex === -1) {
-        return res.status(404).json({ message: 'User not found' });
+        // Enviar a resposta com o usuário atualizado
+        res.status(200).json(users[userIndex]);
+
+
+    } catch (error) {
+        res.status(500).json({ message: 'Internal Server Error', error: error.message })
     }
 
-    // Atualizar os campos do usuário
-    users[userIndex].firstname = firstname;
-    users[userIndex].lastname = lastname;
-
-    // Enviar a resposta com o usuário atualizado
-    res.status(200).json(users[userIndex]);
 };
 
 
-exports.delete=(req,res,next)=>{
-    const id = parseInt(req.params.id);
-    const userIndex = users.findIndex(user => user.id === id);
+exports.delete = (req, res, next) => {
+    try {
+        const id = parseInt(req.params.id);
+        const userIndex = users.findIndex(user => user.id === id);
 
-    users.splice(userIndex, 1);
+        users.splice(userIndex, 1);
 
-    res.status(204).send();
+        res.status(204).send();
+    } catch (error) {
+        res.status(500).json({ message: 'Internal Server Error', error: error.message })
+    }
+
 }
